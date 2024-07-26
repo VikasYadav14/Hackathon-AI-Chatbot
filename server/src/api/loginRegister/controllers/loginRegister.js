515936d764d31ee7ad3632dab12532c8785829a5
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2024-07-26 13:09:43 +0530

    login

--- server/src/api/loginRegister/controllers/loginRegister.js
+++ server/src/api/loginRegister/controllers/loginRegister.js
@@ -1,5 +1,6 @@
 const db = require('../../../../config/dbConnection')
 const User = require('../../../../config/models/Users');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const jwt_Secret = process.env.JWT_SECRET
@@ -10,13 +11,13 @@ const loginRegister = {
     register: async (req, res) => {
         try {
 
-            const { email, password } = req.body;
+            const { username, password } = req.body;
 
-            let user = await User.findOne({ email });
+            let user = await User.findOne({ username });
             if (user) return res.status(400).json('User already exists.')
 
             user = new User({
-                email,
+                username,
                 password
             })
 
@@ -25,7 +26,7 @@ const loginRegister = {
             // for it sign in write after it get register so 
             const payload = {
                 user: {
-                    id: user.id,
+                    id: user.email,
                 },
             };
 
@@ -37,7 +38,37 @@ const loginRegister = {
             console.log(error);
             throw error;
         }
+    },
+
+    logIn: async (req, res) => {
+        try {
+
+            let {username, password} = req.body;
+
+            let user = await User.findOne({username});
+
+            if(!user)  return res.status(200).json({msg: "user do not exists."});
+
+            let isMatch = await bcrypt.compare(password,user.password);
+            if(!isMatch) return res.status(200).json({msg: "password didn't match. please try again"});
+
+            const payload = {
+                user: {
+                    id: user.email,
+                },
+            };
+
+            const token = await jwt.sign(payload, jwt_Secret, { expiresIn: 3600 })
+            if (!token) throw console.error("no token has been generated.");
+            return res.status(201).json({ token });
+
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     }
+
 
 }
 
